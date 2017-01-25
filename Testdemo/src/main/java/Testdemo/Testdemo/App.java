d26992333eaf10e50cfe3804847dfda2542cf808
roshansukur <roshansukur@192.168.0.17>
2017-01-24 23:09:02 -0700

    Git commit

---
+++ Testdemo/src/main/java/Testdemo/Testdemo/App.java
@@ -0,0 +1,13 @@
+package Testdemo.Testdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
